84c2f12a837541aea5c87c87cceea5797603a8b5
TVPRO20 <66882914+TVPRO20@users.noreply.github.com>
2022-11-01 20:51:59 -0500

    dsd

---
+++ front-shopping-cart-v2/scripts/main.js
@@ -0,0 +1,62 @@
+const serverUrl = 'http://localhost:4000/';
+const itemsPath = 'items/';
+const imagesPath = 'img/';
+
+require('dotenv').config();
+
+// BB.DD
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Conectado a BB.DD'));
+
+// Import Routes
+const itemsRoutes = require('./routes/items');
+const imgRoutes = require('./routes/img');
+const paymentsIntentRoutes = require('./routes/paymentsIntent');
+const ordersRoutes = require('./routes/orders');
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+app.use('/items', itemsRoutes);
+app.use('/img', imgRoutes);
+app.use('/create-payment-intent', paymentsIntentRoutes);
+app.use('/order', ordersRoutes);
+
+// Rutas
+app.get('/', (req, res) => {
+  res.send('HOME!');
+});
+
+// Start
+//app.listen(3000);
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT, function() {
+
+	console.log("Servidor escuchando en el puerto", PORT)
+
+})
+
+
+const http = require("http");
+
+require("dotenv").config();
+
+let port = process.env.PORT;
+let host = process.env.HOST;
+
+let server = http.createServer((req, res) => {
+  console.log("Thanks for the request");
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("You Rock");
+});
+
+server.listen(port, host, () => {
+  console.log(`Server is listening ${host}:${port}`);
+});
